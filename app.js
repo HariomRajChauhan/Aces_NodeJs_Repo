b5ba0760d26d1f3a5de456307b91000ac0eb87c2
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2024-07-10 19:08:27 +0545

    Single blog view ,edit and update data features added

--- app.js
+++ app.js
@@ -28,6 +28,53 @@ app.get("/",async (req,res)=>{
     res.render("./blog/home.ejs",{blogs});
 })
 
+
+app.get("/blog/:id",async (req,res)=>{
+    const id = req.params.id;
+    const blog = await Blog.findById(id);
+    res.render("./blog/blogs.ejs",{blog});
+})
+
+
+app.get("/delete/:id",async (req,res)=>{
+    const id = req.params.id;
+    await Blog.findByIdAndDelete(id);
+    res.redirect("/");
+})
+
+// Route to display the edit form with the current blog data
+app.get("/edit/:id", async (req, res) => {
+    const id = req.params.id;
+    // Correctly fetch the blog post to edit
+    const blog = await Blog.findById(id);
+    // Render the edit form and pass the current blog data
+    res.render("./blog/edit.ejs", { blog }); // Assuming the edit form is at "./blog/edit.ejs"
+});
+
+// Route to handle the submission of the edit form
+app.post("/edit/:id", upload.single('image'), async (req, res) => {
+    const id = req.params.id;
+    const { title, description, subtitle } = req.body;
+    let updateData = {
+        title,
+        description,
+        subtitle,
+    };
+
+    // If a new file is uploaded, add it to the update data
+    if (req.file) {
+        const file = req.file.filename;
+        updateData.image = file;
+    }
+
+    // Update the blog post with the new data
+    await Blog.findByIdAndUpdate(id, updateData);
+    // Redirect to the homepage or to the updated blog post
+    res.redirect("/");
+});
+
+
+
 app.get("/contact",(req,res)=>{
     const address = "Aces workshop";
 
